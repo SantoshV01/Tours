f9c4d63e23159ba8369f08442774d90a8594766c
Santhosh <santhvenkat2601@gmail.com>
2021-10-28 17:38:41 +0530

    'changes'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./Card";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://course-api.com/react-tours-project")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .then(setLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <main>
-        <div className="Title">
-          <h2>
-            Best Tours
-            <div className="Underline"></div>
-          </h2>
-        </div>
-        <Card />
-      </main>
+      {loading ? (
+        <div class="loader"></div>
+      ) : (
+        <main>
+          <div className="Title">
+            <h2>
+              Best Tours
+              <div className="Underline"></div>
+            </h2>
+          </div>
+          <Card data={data} />
+        </main>
+      )}
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,66 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-function Card() {
-  const [description, setDescription] = useState(
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,iure totam non neque vero tempora eligendi excepturi a sed, quamitaque expedita obcaecati. Excepturi, voluptatibus. Laborum ratione at quidem fuga!"
-  );
+function Card({ data }) {
   const [readMore, setReadMore] = useState(true);
+
   return (
     <section>
-      <div className="Card">
-        <img
-          src="https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9yayUyMG5pZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-          alt="NY"
-          className="Image"
-        />
-        <div className="About">
-          <p>Best Of New York In 7 Days Tour</p>
-          <p className="Price">$1,995</p>
+      {data.map((data) => (
+        <div className="Card" key={data.id}>
+          <img src={data.image} alt={data.name} className="Image" />
+
+          <div className="About">
+            <p>{data.name}</p>
+            <p className="Price">${data.price}</p>
+          </div>
+
+          <p className="Description">
+            {readMore ? data.info.substring(0, 150) + "..." : data.info}
+            <a onClick={() => setReadMore(!readMore)} className="ReadMore">
+              {readMore ? "Read More" : "Show Less"}
+            </a>
+          </p>
         </div>
-        <p className="Description">
-          {readMore ? description.substring(0, 150) + "..." : description}
-          <a onClick={() => setReadMore(!readMore)} className="ReadMore">
-            {readMore ? "Read More" : "Show Less"}
-          </a>
-        </p>
-        <button className="DeleteButton">Not Interested</button>
-      </div>
-      <div className="Card">
-        <img
-          src="https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9yayUyMG5pZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-          alt="NY"
-          className="Image"
-        />
-        <div className="About">
-          <p>Best Of New York In 7 Days Tour</p>
-          <p className="Price">$1,995</p>
-        </div>
-        <p className="Description">
-          {readMore ? description.substring(0, 150) + "..." : description}
-          <a onClick={() => setReadMore(!readMore)} className="ReadMore">
-            {readMore ? "Read More" : "Show Less"}
-          </a>
-        </p>
-        <button className="DeleteButton">Not Interested</button>
-      </div>
-      <div className="Card">
-        <img
-          src="https://images.unsplash.com/photo-1506606401543-2e73709cebb4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9yayUyMG5pZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"
-          alt="NY"
-          className="Image"
-        />
-        <div className="About">
-          <p>Best Of New York In 7 Days Tour</p>
-          <p className="Price">$1,995</p>
-        </div>
-        <p className="Description">
-          {readMore ? description.substring(0, 150) + "..." : description}
-          <a onClick={() => setReadMore(!readMore)} className="ReadMore">
-            {readMore ? "Read More" : "Show Less"}
-          </a>
-        </p>
-        <button className="DeleteButton">Not Interested</button>
-      </div>
+      ))}
     </section>
   );
 }
